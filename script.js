e97eabde0772fdd928fd2ca46b79f3e282d672f0
lucasdiego33 <lukinha_candido@hotmail.com>
2024-03-15 15:30:49 -0300

    Projeto slide Animado

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let contador = 1;
+
+
+setInterval(function(){
+    document.getElementById("slide" + contador).checked = true;
+    contador++;
+
+if(contador > 4){
+    contador = 1;
+}
+
+
+}, 3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
